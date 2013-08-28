6d1c4396ada104ff6eee2fc7a0bf3021f397d002
LEETAEHO <tae1560@gmail.com>
2013-08-28 09:40:18 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hyosuck1::Application.config.secret_key_base = 'd1c5d994088dd3a7b64788a092047ae1229a24b3ffb5a04d93ec0ab18970f9a8372dfc6f71eff19751abfab8dca3511afc9c5bd2b3197b29b1c012fec6deddfb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hyosuck1::Application.config.session_store :cookie_store, key: '_hyosuck1_session'
